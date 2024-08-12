7c7c1710f359c13d82b16bf635798eb38ae4aff8
uniboxx <unibox@duck.com>
2024-08-12 13:52:43 +0200

    finished lesson 403 added dashboard

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import { createContext } from 'react';
+import { useLocalStorageState } from '../hooks/useLocalStorageState.js';
+import { useContext } from 'react';
+import { useEffect } from 'react';
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode');
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.className = 'dark-mode';
+      } else {
+        document.documentElement.className = 'light-mode';
+      }
+    },
+    [isDarkMode]
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode(isDark => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined)
+    throw new Error('DarkModeContext was used outside of DarkModeProvider');
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
